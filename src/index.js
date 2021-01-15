fd9fcdfee3e4548d95f731f5424d4c690a86c97c
Fang (MacPro) <faang1980@gmail.com>
2021-01-15 20:10:58 +1300

    try main js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './components/main';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Main />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
