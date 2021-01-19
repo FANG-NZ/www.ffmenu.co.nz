1574d70141c92b8399089d23f2b65fc7a031477f
Fang (MacPro) <faang1980@gmail.com>
2021-01-19 14:22:48 +1300

    add react-sticky lib

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-scroll';
+import {StickyContainer, Sticky} from 'react-sticky';
+
 import CatalogBlock from '../components/catalogblock';
 
 
@@ -27,45 +29,52 @@ export default class HomePage extends Component{
             </div>
 
             {/** START menu list */}
+            <StickyContainer>
             <div className="page-content">
                 <div className="container">
                     <div className="row no-gutters">
 
                         {/** START nav bar */}
                         <div className="col-md-3">         
-                            <nav id="menu-navigation" className="stick-to-content" data-local-scroll>
-                                <ul className="nav nav-menu bg-dark dark">
+                            
+                                <Sticky>{(
+                                    ({style}) => (
+                                        <nav id="menu-navigation" className="stick-to-content" style={style}>
+                                            <ul className="nav nav-menu bg-dark dark">
 
-                                    {_data.map(
-                                        (value, index)=>{
-                                            let _name = value.name.toLowerCase();
-                                                _name = _name.replace(/ /g, "-");
+                                                {_data.map(
+                                                    (value, index)=>{
+                                                        let _name = value.name.toLowerCase();
+                                                            _name = _name.replace(/ /g, "-");
 
-                                            return(
-                                                <li key={value.id}>
-                                                    {/* <a href={"#" + _name}  onClick={ (event) => event.preventDefault() }>{value.name}</a> */}
-                                                    {/* <a href={"#" + _name}  onClick={ (event) => (true) }>{value.name}</a> */}
-                                                    <Link
-                                                        activeClass="active" to={_name} 
-                                                        spy={true} smooth={true} 
-                                                        offset={50} duration={500}
-                                                        style={{cursor: "pointer"}}    
-                                                    >
-                                                        {value.name}
-                                                    </Link>
-                                                </li>
-                                            )
-                                        }
-                                    )}
+                                                        return(
+                                                            <li key={value.id}>
+                                                                {/* <a href={"#" + _name}  onClick={ (event) => event.preventDefault() }>{value.name}</a> */}
+                                                                {/* <a href={"#" + _name}  onClick={ (event) => (true) }>{value.name}</a> */}
+                                                                <Link
+                                                                    activeClass="active" to={_name} 
+                                                                    spy={true} smooth={true} 
+                                                                    offset={50} duration={500}
+                                                                    style={{cursor: "pointer"}}    
+                                                                >
+                                                                    {value.name}
+                                                                </Link>
+                                                            </li>
+                                                        )
+                                                    }
+                                                )}
 
-                                    {/* <li><a href="#/about-us" onClick={ (event) => event.preventDefault() }>Burgers</a></li>
-                                    <li><a href="#Pasta">Pasta</a></li>
-                                    <li><a href="#Pizza">Pizza</a></li>
-                                    <li><a href="#Sushi">Sushi</a></li>
-                                    <li><a href="#Desserts">Desserts</a></li>
-                                    <li><a href="#Drinks">Drinks</a></li> */}
-                                </ul>
-                            </nav>
+                                            {/* <li><a href="#/about-us" onClick={ (event) => event.preventDefault() }>Burgers</a></li>
+                                            <li><a href="#Pasta">Pasta</a></li>
+                                            <li><a href="#Pizza">Pizza</a></li>
+                                            <li><a href="#Sushi">Sushi</a></li>
+                                            <li><a href="#Desserts">Desserts</a></li>
+                                            <li><a href="#Drinks">Drinks</a></li> */}
+                                            </ul>
+                                        </nav>
+                                    )
+                                )}</Sticky>
+                            
                         </div>
 
                         {/** START catalog & meals */}
@@ -82,7 +91,7 @@ export default class HomePage extends Component{
                     </div>
                 </div>
             </div>
-
+            </StickyContainer>
             </>   
         );
     }
