c6c6583fef31a9708a30353ecf102a5ae1c217a5
Fang (MacPro) <faang1980@gmail.com>
2021-01-30 15:05:07 +1300

    update new about us & contact us page

--- src/pages/aboutuspage.jsx
+++ src/pages/aboutuspage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {animated} from 'react-spring/renderprops';
 
+import bgImage from '../images/about-us-bg.jpg';
+
 export default class AboutUsePage extends Component{
 
     render(){
@@ -9,18 +11,53 @@ export default class AboutUsePage extends Component{
         return(
             <animated.div 
                 className="page-animate"
-                style={{..._style}}  
+                style={_style}
             >
             <div className="page-title bg-light">
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-8 offset-lg-4">
-                            <h1 className="mb-0">About Us Page</h1>
-                            <h4 className="text-muted mb-0">We are in ABOUT US PAGE</h4>
+                            <h1 className="mb-0">About Us</h1>
+                            <h4 className="text-muted mb-0">We provide the best for you</h4>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <section className="section section-bg-edge">
+
+                <div className="image left bottom col-md-7">
+                    <div className="bg-image">
+                        <img src={bgImage} alt="" />
+                    </div>
+                </div>
+
+            <div className="container">
+                <div className="col-lg-5 offset-lg-5 col-md-9 offset-md-3">
+                    <div className="rate mb-5 rate-lg">
+                        <i className="fa fa-star active"></i>
+                        <i className="fa fa-star active"></i>
+                        <i className="fa fa-star active"></i>
+                        <i className="fa fa-star active"></i>
+                        <i className="fa fa-star active"></i>
+                    </div>
+                    <h2>The best food in Hamilton</h2>
+                    <p className="">
+                        Kokoro Sushi has been passionately committed since our beginning to being 
+                        unlike any other sushi experience available. Our progressive approach to sushi 
+                        pays homage to the artisanal beauty of traditional sushi, while also pushing the 
+                        boundaries by combining sushi with other global cuisines and flavor profiles
+                    </p>
+                    <p>
+                        We strive to be different, and we have a unique vision of what sushi can be. 
+                        We don’t just do the common rolls, we do rolls our way
+                    </p>
+                    <h6>Team of KOKORO SUSHI</h6>
+                    
+                </div>
+            </div>
+
+            </section>
             </animated.div>
         );
     }
--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -9,18 +9,50 @@ export default class ContactPage extends Component{
         return(
             <animated.div 
                 className="page-animate"
-                style={{..._style}}  
+                style={_style}  
             >
             <div className="page-title bg-light">
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-8 offset-lg-4">
-                            <h1 className="mb-0">Contact Page</h1>
-                            <h4 className="text-muted mb-0">Hello CONTACT PAGE</h4>
+                            <h1 className="mb-0">Contact Us</h1>
+                            <h4 className="text-muted mb-0">(9:30 - 15:30 Mon - Sat)</h4>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <section class="section">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-4 offset-lg-1 col-md-6 mb-5 mb-md-0">
+                            {/* <img src="assets/img/logo-horizontal-dark.svg" alt="" class="mb-5" width="130" /> */}
+                            <h4 className="mb-0">KOKORO SUSHI</h4>
+                            <span className="text-muted">2B Gordonton Road, Hamilton</span>
+                            <hr className="hr-md" />
+                            <div className="row">
+                                <div className="col-sm-6 mb-3 mb-sm-0">
+                                    <h6 className="mb-1 text-muted">Phone:</h6>
+                                    +64 856 1234
+                                </div>
+                                <div className="col-sm-6">
+                                    <h6 className="mb-1 text-muted">E-mail:</h6>
+                                    <a href="mailto:hello@kokorosushi.co.nz">hello@kokorosushi.co.nz</a>
+                                </div>
+                            </div>
+                            <hr className="hr-md" />
+                            <h6 className="mb-3 text-muted">Follow Us!</h6>
+                            <a href="#" className="icon icon-social icon-circle icon-sm icon-facebook">
+                                <i className="fa fa-facebook"></i>
+                            </a>
+                        </div>
+
+                        <div className="col-lg-5 offset-lg-2 col-md-6">
+                            <div className="google-map h-500 shadow" data-lat="50.064651" data-lon="19.944981"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
             </animated.div>
         );
     }
